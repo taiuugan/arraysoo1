33f0668b4002fd67126d21f2377c14d03e008361
taiuugan <taielcrak@gmail.com>
2026-04-24 21:42:42 -0300

    agregamos js

---
+++ js.js
@@ -0,0 +1,10 @@
+let calificaciones = [
+    {
+        nombre: "juan" ,
+        materia: "matematica",
+        primernota: 5,
+        segundanota: 7,
+        terceranota: 9,
+        promedio: 7
+    }
+];
